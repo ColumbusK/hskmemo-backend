6b4a229417d1c9c6b18e9626c9a7f1dda8304b8d
morse <zkzkao@foxmail.com>
2024-02-19 16:51:02 +0800

    2024/02/19

--- api/index.route.js
+++ api/index.route.js
@@ -4,7 +4,7 @@ import express from 'express';
 const router = express.Router();
 
 router.route('/').get((req, res, next) => {
-  res.send('hello world');
+  res.render('index', { title: 'WeApp Backend' });
 });
 
 export default router;
